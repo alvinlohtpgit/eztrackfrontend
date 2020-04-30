24d8affbe81b6328bf0ccd8f7145bb2eda9de948
alvinloh <M8S24Vds>
2020-04-30 14:11:12 +0800

    Sign Up Fields

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -7,10 +7,19 @@
                  <v-container class="mt-8">
                      <v-row no-gutters>
                          <v-col cols="3">
+                            Email
+                         </v-col>
+                         <v-col cols="8">
+                             <v-text-field v-model='signup_email' filled dense label='You will use this email to sign in to ezTrack'></v-text-field>
+                         </v-col>
+                     </v-row>
+
+                     <v-row no-gutters>
+                         <v-col cols="3">
                             First Name
                          </v-col>
                          <v-col cols="8">
-                             <v-text-field filled dense></v-text-field>
+                             <v-text-field v-model='signup_firstname' filled dense></v-text-field>
                          </v-col>
                      </v-row>
 
@@ -19,25 +28,17 @@
                             Last Name
                          </v-col>
                          <v-col cols="8">
-                             <v-text-field filled dense></v-text-field>
+                             <v-text-field v-model='signup_lastname' filled dense></v-text-field>
                          </v-col>
                      </v-row>
 
-                    <v-row no-gutters>
-                         <v-col cols="3">
-                            Email
-                         </v-col>
-                         <v-col cols="8">
-                             <v-text-field filled dense></v-text-field>
-                         </v-col>
-                     </v-row>
 
                      <v-row no-gutters>
                          <v-col cols="3">
                             Password
                          </v-col>
                          <v-col cols="8">
-                             <v-text-field filled dense></v-text-field>
+                             <v-text-field v-model='signup_password1' filled dense></v-text-field>
                          </v-col>
                      </v-row>
 
@@ -46,14 +47,14 @@
                             Password Again
                          </v-col>
                          <v-col cols="8">
-                             <v-text-field filled dense></v-text-field>
+                             <v-text-field v-model='signup_password2' filled dense></v-text-field>
                          </v-col>
                      </v-row>
 
                       <v-row no-gutters>
                          <v-col cols="11">
                              <div class="d-flex justify-end">
-                                 <v-btn color="primary">Sign Up</v-btn>
+                                 <v-btn color="primary" @click='dosignup'>Sign Up</v-btn>
                              </div>
                          </v-col>
                      </v-row>
@@ -103,8 +104,20 @@
 export default {
     name: 'SignIn',
     data: () => ({
-
+        signup_email: null,
+        signup_password1: null,
+        signup_password2: null,
+        signup_firstname: null,
+        signup_lastname: null,
+        email_already_exist: false,
     }),
+    methods:{
+        dosignup: function(){
+            // Check that the email doesn't exist 
+           // alert(process.env.VUE_APP_SERVERPATH);
+           
+        }
+    }
 }
 </script>
 
